5f55ca1c688b0f51b234b5a6f0d036c66d1afb30
subru <easwaransubramaniOO7@gmail.com>
2023-09-24 03:42:24 +0530

    added extra body elements for goal endpoint

--- src/controllers/goal.controller.js
+++ src/controllers/goal.controller.js
@@ -1,81 +1,98 @@
-const { getAllGoalsService, getGoalByIdService, addNewGoalService } = require("../services/goal.service");
+const {
+  getAllGoalsService,
+  getGoalByIdService,
+  addNewGoalService,
+} = require("../services/goal.service");
 
 const getAllGoalsContoller = async (req, res) => {
-    const user = req?.user;
+  const user = req?.user;
 
-    if (!user || user === '' || user === undefined) {
-        return res.status(400).send('User is required');
-    }
+  if (!user || user === "" || user === undefined) {
+    return res.status(400).send("User is required");
+  }
 
-    try {
-        result = await getAllGoalsService(user);
-        if (result) {
-            res.status(200).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err.message);
+  try {
+    result = await getAllGoalsService(user);
+    if (result) {
+      res.status(200).send(result);
     }
-}
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
 const getGoalByIdController = async (req, res) => {
-    const user = req?.user;
-    const goalId = req?.params?.id;
+  const user = req?.user;
+  const goalId = req?.params?.id;
 
-    if (!user || user === '' || user === undefined) {
-        return res.status(400).send('User is required');
+  if (!user || user === "" || user === undefined) {
+    return res.status(400).send("User is required");
+  }
+
+  if (!goalId || goalId === "" || goalId === undefined) {
+    return res.status(400).send("Goal Id is required");
+  }
+
+  try {
+    result = await getGoalByIdService(user, goalId);
+    if (result) {
+      console.log(result);
+      res.status(200).send(result);
     }
-
-
-    if (!goalId || goalId === '' || goalId === undefined) {
-        return res.status(400).send('Goal Id is required');
-    }
-
-    try {
-        result = await getGoalByIdService(user, goalId);
-        if (result) {
-            console.log(result);
-            res.status(200).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-}
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
 const addNewGoalController = async (req, res) => {
-    const user = req?.user;
-    const title = req?.body?.goal?.title;
-    const amount = req?.body?.goal?.amount;
-    const targetDate = req?.body?.goal?.targetDate;
+  const user = req?.user;
+  const title = req?.body?.goal?.title;
+  const amount = req?.body?.goal?.amount;
+  const targetDate = req?.body?.goal?.targetDate;
+  const currentSaved = req?.body?.goal?.currentSaved;
+  const priority = req?.body?.goal?.priority;
+  // current saved
+  // priority
 
-    if (!user || user === '' || user === undefined) {
-        return res.status(400).send('User is required');
+  if (!user || user === "" || user === undefined) {
+    return res.status(400).send("User is required");
+  }
+  if (!title || title === "" || title === undefined) {
+    return res.status(400).send("Title is required");
+  }
+  if (!amount || amount === "" || amount === undefined) {
+    return res.status(400).send("Amount is required");
+  }
+  if (!targetDate || targetDate === "" || targetDate === undefined) {
+    return res.status(400).send("Target date is required");
+  }
+  if (!currentSaved || currentSaved === "" || currentSaved === undefined) {
+    return res.status(400).send("Target date is required");
+  }
+  if (!priority || priority === "" || priority === undefined) {
+    return res.status(400).send("Target date is required");
+  }
+
+  try {
+    result = await addNewGoalService(
+      user,
+      title,
+      amount,
+      targetDate,
+      currentSaved,
+      priority
+    );
+
+    if (result) {
+      res.status(200).send(result);
     }
-    if (!title || title === '' || title === undefined) {
-        return res.status(400).send('Title is required');
-    }
-    if (!amount || amount === '' || amount === undefined) {
-        return res.status(400).send('Amount is required');
-    }
-    if (!targetDate || targetDate === '' || targetDate === undefined) {
-        return res.status(400).send('Target date is required');
-    }
-
-    try {
-        result = await addNewGoalService(user, title, amount, targetDate);
-
-        if (result) {
-
-            res.status(200).send(result);
-        }
-    } catch (err) {
-
-        res.status(400).send(err.message);
-    }
-}
-
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
 module.exports = {
-    getAllGoalsContoller,
-    getGoalByIdController,
-    addNewGoalController
-}
+  getAllGoalsContoller,
+  getGoalByIdController,
+  addNewGoalController,
+};
